c4c9c08e53a5881a3ad8991453f74dc6de67a16b
m-triple-m <triplem656@gmail.com>
2025-03-11 08:58:06 +0530

    added preline

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import PrelineScript from "@/components/PrelineScript";
-import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
